9463e180786d86194760a8e60e0eab72b71a80ad
inferno-op <ivar@inferno-op.com>
2015-07-19 19:34:24 +0200

    Added 'Fluid Malone' Noise

--- the_noise.glsl
+++ the_noise.glsl
@@ -13,12 +13,17 @@
 // Crawling Noise based on https://www.shadertoy.com/view/lslXRS by nimitz
 // Cells on Fire based on https://www.shadertoy.com/view/lsX3z4 by JoshP
 // Water Noise based on https://www.shadertoy.com/view/llsGWl by guil
- 
+// Fluid Malone Noise based on https://www.shadertoy.com/view/4s23WK by Antonalog
+
 // License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 
 #define PI 3.14159265359
 #define tau 6.2831853
 #define r_iter 6.
+
+/* skew constants for 3d simplex functions */
+const float F3 =  0.3333333;
+const float G3 =  0.1666667;
 
 uniform float adsk_result_w, adsk_result_h, adsk_time, adsk_result_frameratio;
 vec2 resolution = vec2(adsk_result_w, adsk_result_h);
@@ -92,6 +97,10 @@ uniform int cof_noise_type;
 // Water uniforms
 uniform float water_detail;
 uniform float w_noise;
+
+// Malone uniforms
+uniform float malone_size, malone_swirl; 
+uniform int malone_detail;
 
 // start concrete noise
 float hash ( in vec2 p ) 
@@ -724,6 +733,132 @@ float water_map(vec2 p){
 }
 // end water noise
 
+// start malone noise
+vec2 Rot(vec2 p, float t) {
+	float c = cos(t); float s = sin(t);
+	return vec2(p.x*c+p.y*s,
+				-p.x*s+p.y*c);
+}
+vec2 RotCS(vec2 p, float c, float s) {
+	return vec2( p.x*c+p.y*s,
+				-p.x*s+p.y*c);
+}
+
+//iq 2d simplex noise
+vec2 malone_hash( vec2 p )
+{
+	p = vec2( dot(p,vec2(127.1,311.7)),
+			  dot(p,vec2(269.5,183.3)) );
+	vec2 h = -1.0 + 2.0*fract(sin(p)*43758.5453123);
+	//extra rotations for more flow!
+	float t = -time*0.7;
+	float co = cos(t); float si = sin(t);	
+	h = RotCS(h,co,si);
+	return h;
+}
+
+float malone_noise( in vec2 p )
+{
+    const float K1 = 0.366025404; // (sqrt(3)-1)/2;
+    const float K2 = 0.211324865; // (3-sqrt(3))/6;
+	vec2 i = floor( p + (p.x+p.y)*K1 );
+    vec2 a = p - i + (i.x+i.y)*K2;
+    vec2 o = (a.x>a.y) ? vec2(1.0,0.0) : vec2(0.0,1.0);
+    vec2 b = a - o + K2;
+	vec2 c = a - 1.0 + 2.0*K2;
+	float t = time*.5;
+	float co = cos(t); float si = sin(t);	
+	a = RotCS(a,co,si);
+	b = RotCS(b,co,si);
+	c = RotCS(c,co,si);
+    vec3 h = max( 0.5-vec3(dot(a,a), dot(b,b), dot(c,c) ), 0.0 );
+	vec3 n = h*h*h*h*vec3( dot(a,malone_hash(i+0.0)), dot(b,malone_hash(i+o)), dot(c,malone_hash(i+1.0)));
+    return dot( n, vec3(100.) );
+}
+
+vec3 random3(vec3 c) {
+	float j = 4096.0*sin(dot(c,vec3(17.0, 59.4, 15.0)));
+	vec3 r;
+	r.z = fract(512.0*j);
+	j *= .125;
+	r.x = fract(512.0*j);
+	j *= .125;
+	r.y = fract(512.0*j);
+	r = r-0.5;
+	//rotate for extra flow!
+	float t = -time*.5;
+	r.xy = Rot(r.xy,t);
+	return r;
+}
+
+/* 3d simplex noise */
+float malone_noise(vec3 p) {
+	 /* 1. find current tetrahedron T and its four vertices */
+	 /* s, s+i1, s+i2, s+1.0 - absolute skewed (integer) coordinates of T vertices */
+	 /* x, x1, x2, x3 - unskewed coordinates of p relative to each of T vertices*/
+	 
+	 /* calculate s and x */
+	 vec3 s = floor(p + dot(p, vec3(F3)));
+	 vec3 x = p - s + dot(s, vec3(G3));
+	 
+	 /* calculate i1 and i2 */
+	 vec3 e = step(vec3(0.0), x - x.yzx);
+	 vec3 i1 = e*(1.0 - e.zxy);
+	 vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+	 	
+	 /* x1, x2, x3 */
+	 vec3 x1 = x - i1 + G3;
+	 vec3 x2 = x - i2 + 2.0*G3;
+	 vec3 x3 = x - 1.0 + 3.0*G3;
+	 	
+	 /* 2. find four surflets and store them in d */
+	 vec4 w, d;
+	 
+	 /* calculate surflet weights */
+	 w.x = dot(x, x);
+	 w.y = dot(x1, x1);
+	 w.z = dot(x2, x2);
+	 w.w = dot(x3, x3);
+	 
+	 /* w fades from 0.6 at the center of the surflet to 0.0 at the margin */
+	 w = max(0.6 - w, 0.0);
+	 
+	 /* calculate surflet components */
+	 d.x = dot(random3(s), x);
+	 d.y = dot(random3(s + i1), x1);
+	 d.z = dot(random3(s + i2), x2);
+	 d.w = dot(random3(s + 1.0), x3);
+	 
+	 /* multiply d by w^4 */
+	 w *= w;
+	 w *= w;
+	 d *= w;
+	 
+	 /* 3. return the sum of the four surflets */
+	 return dot(d, vec4(52.0));
+}
+
+float pot(vec2 pos)
+{
+	vec3 p = vec3(pos,1.0);
+	float n = malone_noise(p);
+	n += 0.5 *malone_noise(p*2.13);
+	n += 3. * malone_noise(pos*0.333);
+	return n;
+}
+
+vec2 field(vec2 pos)
+{
+	float s = 1.0 * malone_swirl;
+	pos *= s;
+	float n = pot(pos);
+	float e = 0.1;
+	float nx = pot(vec2(pos+vec2(e,0.)));
+	float ny = pot(vec2(pos+vec2(0.,e)));
+	return vec2(-(ny-n),nx-n)/e;
+}
+// end malone noise
+
 void main()
 {
 	vec2 uv = (gl_FragCoord.xy / resolution.xy) - pos;
@@ -944,6 +1079,23 @@ void main()
 	   	uv += m * time * .1;
 	    float k = water_map(uv)*.8+.25;
 		col.rgb = vec3(clamp(k, 0.0, 1.0));
+	}
+	
+	else if ( noise_type == 18 )
+	{
+		uv.x += 50.5;
+		uv *= 0.25;
+		vec2 src_uv = uv;
+		vec3 d = vec3(0.);
+		for (int i=0; i<malone_detail; i++)
+		{
+			vec2 new_uv = field(uv)*.00825*.5;
+			uv += new_uv;
+		}
+		col.rgb = vec3(malone_noise(uv*malone_size));
+		col.rgb = 0.5 + 0.5 * col.rgb;
+		col.rgb = clamp(col.rgb, 0.0, 1.0);
+
 	}
 	
 	gl_FragColor = col;
